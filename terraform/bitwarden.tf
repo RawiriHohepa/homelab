9052885b80cb5457f140bc78513ca3de15599289
Rawiri Hohepa <rawirihohepa@gmail.com>
2026-09-19 20:06:36 +1200

    Use Bitwarden Secrets Manager for tailscale oauth creds

--- terraform/bitwarden.tf
+++ terraform/bitwarden.tf
@@ -12,6 +12,19 @@ resource "bitwarden-secrets_secret" "cloudflare_access_token_dns" {
   note = "Cloudflare access token with Zone Read and DNS Write permissions."
 }
 
+# https://tailscale.com/docs/kubernetes-operator/quickstart#create-oauth-credentials-and-configure-secrets
+resource "bitwarden-secrets_secret" "tailscale_client_id" {
+  key = "tailscale-client-id"
+  value = data.sops_file.secrets.data["secrets.tailscale-client-id"]
+  project_id = var.bitwarden_project_id
+}
+
+resource "bitwarden-secrets_secret" "tailscale_client_secret" {
+  key = "tailscale-client-secret"
+  value = data.sops_file.secrets.data["secrets.tailscale-client-secret"]
+  project_id = var.bitwarden_project_id
+}
+
 # # Generate a new value
 # resource "bitwarden-secrets_secret" "db_admin_password" {
 #   key         = "db_admin_password"
